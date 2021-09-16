7852e4064ad38b244aa97dac12f362a563087a13
divinevibration <83971946+divinevibration@users.noreply.github.com>
2021-09-16 15:25:54 +0200

    commite

--- mail.php
+++ mail.php
@@ -1,16 +1,19 @@
 <?php
-//get data from form  
+//get data from form
 $name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "sabelogmkhwanazi05@mail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@https://innovatius.netlify.app/" . "\r\n" .
-"CC: sabelo.mailme@gmail.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+$email = $_POST['email'];
+$message = $_POST['message'];
+$to = 'sabelogmkhwanazi05@gmail.com';
+$subject = 'Mail From Innovatius';
+$txt =
+    'Name = ' . $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$headers =
+    'From: noreply@https://innovatius.netlify.app/' .
+    "\r\n" .
+    'CC: sabelo.mailme@gmail.com';
+if ($email != null) {
+    mail($to, $subject, $txt, $headers);
 }
 //redirect
-header("Location:thankyou.html");
+header('Location:thankyou.html');
 ?>
